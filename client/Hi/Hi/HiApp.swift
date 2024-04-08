3090923af31816cd24ba772917993c2324a4172d
Tsuri Yuma <yuma33t@gmail.com>
2024-04-08 20:56:04 +0900

    add: プロジェクト作成

---
+++ client/Hi/Hi/HiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiApp.swift
+//  Hi
+//
+//  Created by Yuma on 2024/04/08.
+//
+
+import SwiftUI
+
+@main
+struct HiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
