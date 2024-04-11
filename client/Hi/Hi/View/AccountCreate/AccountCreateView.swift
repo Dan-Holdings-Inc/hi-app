75d27ac100f6005d81ac09a5247207efedc2054f
Tsuri Yuma <yuma33t@gmail.com>
2024-04-12 00:29:55 +0900

    refactor: アカウント作成画面のファイルを整理した

---
+++ client/Hi/Hi/View/AccountCreate/AccountCreateView.swift
@@ -0,0 +1,56 @@
+//
+//  AccountCreateView.swift
+//  Hi
+//
+//  Created by Yuma on 2024/04/11.
+//
+
+import SwiftUI
+
+struct AccountCreateNameView: View {
+    @EnvironmentObject var router: NavigationRouter
+    
+    var body: some View {
+        AccountSettingName(nextButtonLabel: "次へ", isShowBackButton: false, action: {
+            router.navigateToView(destination: .accountCreateUserID)
+        })
+        .navigationBarHidden(true)
+    }
+}
+
+struct AccountCreateUserID: View {
+    @EnvironmentObject var router: NavigationRouter
+    
+    var body: some View {
+        AccountSettingUserID(nextButtonLabel: "次へ", action: {
+            router.navigateToView(destination: .accountCreateWakeUpTime)
+        })
+        .navigationBarHidden(true)
+    }
+}
+
+struct AccountCreateWakeUpTimeView: View {
+    @EnvironmentObject var router: NavigationRouter
+    
+    var body: some View {
+        AccountSettingWakeUpTime(nextButtonLabel: "次へ", action: {
+            router.navigateToView(destination: .accountCreateDayOfWeek)
+        })
+        .navigationBarHidden(true)
+    }
+}
+
+struct AccountCreateDayOfWeekView: View {
+    @EnvironmentObject var router: NavigationRouter
+    
+    var body: some View {
+        AccountSettingDayOfWeek(nextButtonLabel: "Hiを始める！", action: {
+            router.navigateToView(destination: .main)
+        })
+        .navigationBarHidden(true)
+    }
+}
+
+#Preview {
+    AccountCreateNameView()
+}
