7290589a6c71dbc932f7873898185d23d1244f1d
Tsuri Yuma <yuma33t@gmail.com>
2024-04-12 17:52:09 +0900

    feat: フレンド承認画面を追加

---
+++ client/Hi/Hi/View/Friend/FriendApprovalView.swift
@@ -0,0 +1,28 @@
+//
+//  FriendApprovalView.swift
+//  Hi
+//
+//  Created by Yuma on 2024/04/12.
+//
+
+import SwiftUI
+
+struct FriendApprovalView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                BackButton()
+                Text("フレンド承認待ち")
+                    .font(.title)
+                    .bold()
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    FriendApprovalView()
+}
