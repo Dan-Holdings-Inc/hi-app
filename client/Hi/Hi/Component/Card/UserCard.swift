77fbe76d42d3e45c9586ea6b58e06c65085c9f2c
ryosei15 <ryosei.315914.sari@gmail.com>
2024-04-16 10:32:12 +0900

    ホーム画面変更

--- client/Hi/Hi/Component/Card/UserCard.swift
+++ client/Hi/Hi/Component/Card/UserCard.swift
@@ -11,22 +11,34 @@ struct UserCard: View {
     var userName: String
     var color: Color
     var action: () -> Void
+    @State private var scale: CGFloat = 1.0
     
     var body: some View {
         let screenWidth = UIScreen.main.bounds.width
         
-        Button(action: action) {
+        Button(action: {
+            withAnimation {
+                scale = 0.95
+                action()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    withAnimation{
+                        scale = 1.0
+                    }
+                }
+            }
+        }) {
             Text("\(userName)")
                 .foregroundColor(.white)
                 .font(.title)
                 .bold()
                 .padding()
-            
                 .frame(width: screenWidth, height: 80)
                 .background(color)
+                .scaleEffect(scale)
         }
     }
 }
+
 
 #Preview {
     UserCard(userName: "だん", color: .blue, action: {
--- client/Hi/Hi/View/Home/HomeView.swift
+++ client/Hi/Hi/View/Home/HomeView.swift
@@ -9,21 +9,34 @@ import SwiftUI
 
 struct HomeView: View {
     @ObservedObject var viewModel: HomeViewModel
+    @State private var searchText = ""
     
     var body: some View {
         List {
-            ForEach(0 ..< 10) { index in
-                UserCard(userName: "\(index)番目の人", color: viewModel.cardColors[index % viewModel.cardColors.count], action: {
-                    viewModel.userCardButtonAction(index: index)
-                })
-                .swipeActions(edge: .trailing){
-                    Button("削除", role: .destructive) {
+            Section(header:
+                        HStack {
+                TextField("検索", text: $searchText)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 12)
+                    .background(Color(.systemGray5))
+                    .cornerRadius(8)
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                
+            }
+            ){
+                ForEach(0 ..< 10) { index in
+                    UserCard(userName: "\(index)番目の人", color: viewModel.cardColors[index % viewModel.cardColors.count], action: {
+                        viewModel.userCardButtonAction(index: index)
+                    })
+                    .swipeActions(edge: .trailing){
+                        Button("削除", role: .destructive) {
+                        }
                     }
+                    .listRowInsets(EdgeInsets())
                 }
-                .listRowInsets(EdgeInsets())
             }
         }.listStyle(GroupedListStyle())
-        
     }
 }
 
