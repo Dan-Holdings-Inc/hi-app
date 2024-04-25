82c93d06367074e918d7ec816a22da0f433074e1
fujisaki <fujidan8464@gmail.com>
2024-04-25 11:33:28 +0900

    WIP AWS sns

--- server/src/controllers/hi/hi.controller.ts
+++ server/src/controllers/hi/hi.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Post } from "@nestjs/common";
+import { SNS } from "aws-sdk";
 
 // @UseGuards(AuthGuard("jwt"))
 @Controller("hi")
 export class HiController {
   @Post()
-  async sendHi() {}
+  async sendHi() {
+    // SNS()
+  }
 }
--- server/src/services/hi/hi.service.ts
+++ server/src/services/hi/hi.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class HiService {}
+export class HiService {
+  sendHi() {}
+}
