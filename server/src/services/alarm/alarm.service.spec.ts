3fb2174cb6cf3cdc4b129ae48fcbe573def18a19
fujisaki <fujidan8464@gmail.com>
2024-04-26 20:49:23 +0900

    WIP: alarmのテスト中

--- server/src/services/alarm/alarm.service.spec.ts
+++ server/src/services/alarm/alarm.service.spec.ts
@@ -1,9 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AlarmService } from "./alarm.service";
-import { DbService } from "src/infrastructure/db/db.service";
-import { HiService } from "../hi/hi.service";
+
 import { Alarm } from "src/entity/entities/alarm";
 import { User } from "src/entity/entities/user";
+import * as dayjs from "dayjs";
+const TIMEZONE = "Asia/Tokyo";
+import * as timezone from "dayjs/plugin/timezone";
+import * as utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+//
 
 describe("AlarmService", () => {
   let service: AlarmService;
@@ -11,7 +18,7 @@ describe("AlarmService", () => {
     _id: "euiahfoweh9120e93",
     userId: "ioqweuwoeur9",
     getUpAt: "10:00",
-    daysToAlarm: [true, false, false, true, true, false, true],
+    daysToAlarm: [false, true, false, true, true, false, true],
   };
 
   const user: User = {
@@ -23,7 +30,7 @@ describe("AlarmService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AlarmService, DbService, HiService],
+      providers: [AlarmService],
     }).compile();
 
     service = module.get<AlarmService>(AlarmService);
@@ -34,8 +41,19 @@ describe("AlarmService", () => {
   });
 
   describe("calculateAlarmDate", () => {
-    it("calculate ", () => {
-      expect(AlarmService.calculateAlarmDate(alarm).toISOString()).toBe("");
+    it("should be on a same day ", () => {
+      const now = dayjs.tz("2024-04-01T09:00:00", TIMEZONE);
+
+      expect(AlarmService.calculateAlarmDate(now, alarm).toISOString()).toBe(
+        "2024-04-01T01:00:00.000Z"
+      );
+    });
+
+    it("should be on wednesday1", () => {
+      const now = dayjs.tz("2024-04-01T10:00:01", TIMEZONE);
+      expect(AlarmService.calculateAlarmDate(now, alarm).toISOString()).toBe(
+        "2024-04-03T01:01:00.000Z"
+      );
     });
   });
 });
--- server/src/services/alarm/alarm.service.ts
+++ server/src/services/alarm/alarm.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from "@nestjs/common";
 import { Alarm } from "../../entity/entities/alarm";
-import { DbService } from "src/infrastructure/db/db.service";
-import { HiService } from "../hi/hi.service";
-import dayjs, { Dayjs } from "dayjs";
-import { TIMEZONE } from "src/utils";
-
+import * as dayjs from "dayjs";
+import { TIMEZONE } from "../../utils";
+import * as timezone from "dayjs/plugin/timezone";
+import * as utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+dayjs.extend(timezone);
 @Injectable()
 export class AlarmService {
-  constructor(
-    private dbService: DbService,
-    private hiService: HiService
-  ) {}
+  constructor() {}
 
   init() {}
 
   /**
    * 次のアラームを設定する日時を計算します。
    */
-  static calculateAlarmDate(alarm: Alarm) {
-    const today: Dayjs = dayjs().tz(TIMEZONE);
+  static calculateAlarmDate(now: dayjs.Dayjs, alarm: Alarm) {
     const timeRegex = /(\d\d?):(\d\d?)/;
     const time = alarm.getUpAt.match(timeRegex);
     const hour = Number(time[1]);
@@ -26,20 +23,24 @@ export class AlarmService {
     //次回のアラーム曜日
     let howManyDaysAfter = 0;
     let index = 0;
-    if (alarm.daysToAlarm[today.day()]) {
-      const expectedDate = today.set("hour", hour).set("minute", minute);
-      const passedAlready = today.unix() - expectedDate.unix() > 0;
+    if (alarm.daysToAlarm[now.day()]) {
+      const expectedDate = now.set("hour", hour).set("minute", minute);
+      const passedAlready = now.unix() - expectedDate.unix() > 0;
       howManyDaysAfter = passedAlready ? 1 : 0;
-      index = passedAlready ? (today.day() + 1) % 7 : today.day();
+      index = passedAlready ? (now.day() + 1) % 7 : now.day();
     } else {
       howManyDaysAfter = 1;
-      index = (today.day() + 1) % 7;
+      index = (now.day() + 1) % 7;
     }
     while (!alarm.daysToAlarm[index]) {
       howManyDaysAfter += 1;
       index = (index + 1) % 7;
     }
-    const date = today.add(howManyDaysAfter, "day");
+    const date = now
+      .add(howManyDaysAfter, "day")
+      .tz(TIMEZONE)
+      .set("hour", hour)
+      .set("minute", minute);
     return date;
   }
 }
@@ -51,7 +52,7 @@ class AlarmStore {
 
   remove(key: string) {}
 
-  static buildKey(day: Dayjs) {
+  static buildKey(day: dayjs.Dayjs) {
     return day.format("YYYY/MM/DD HH:mm:ss");
   }
 }
