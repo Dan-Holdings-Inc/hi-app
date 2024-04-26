9b236d4ade2a90b346ce78a3f326fd23deb306d7
fujisaki <fujidan8464@gmail.com>
2024-04-26 21:25:08 +0900

    クエリパラメータで検索できる方が適切だと思い、修正しました

--- server/src/controllers/search/search.controller.ts
+++ server/src/controllers/search/search.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from "@nestjs/common";
 import { UsersService } from "src/services/users/users.service";
 // @UseGuards(AuthGuard("jwt"))
 @Controller("search")
 export class SearchController {
   constructor(private usersService: UsersService) {}
 
-  @Get("users/:identifier")
-  async searchUsers(@Param("identifier") identifier: string) {
-    return await this.usersService.search(identifier);
+  @Get("users")
+  async searchUsers(@Query() query) {
+    const text = query.text;
+    if (!text) {
+      throw new BadRequestException("query parameter 'text' is required.");
+    }
+    return await this.usersService.search(text);
   }
 }
