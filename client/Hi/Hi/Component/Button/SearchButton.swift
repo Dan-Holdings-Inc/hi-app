26f84d81e95240669f77b5f204fc5a9d74653887
ryosei15 <ryosei.315914.sari@gmail.com>
2024-04-28 02:15:39 +0900

    検索バー修正

---
+++ client/Hi/Hi/Component/Button/SearchButton.swift
@@ -0,0 +1,18 @@
+//
+//  SearchButton.swift
+//  Hi
+//
+//  Created by ryosei on 2024/04/27.
+//
+
+import SwiftUI
+
+struct SearchButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchButton()
+}
