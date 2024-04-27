3447d8c9911d3809546829122f14c106c36aa280
ryosei15 <ryosei.315914.sari@gmail.com>
2024-04-27 15:44:52 +0900

    設定変更

---
+++ client/Hi/Hi/Model/UserChangeDto.swift
@@ -0,0 +1,8 @@
+//
+//  UserChangeDto.swift
+//  Hi
+//
+//  Created by ryosei on 2024/04/26.
+//
+
+import Foundation
---
+++ client/Hi/Hi/View/Account/Setting/SettingModel.swift
@@ -0,0 +1,18 @@
+//
+//  SettingModel.swift
+//  Hi
+//
+//  Created by ryosei on 2024/04/26.
+//
+
+import SwiftUI
+
+struct SettingModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingModel()
+}
