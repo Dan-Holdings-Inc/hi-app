44fe7b37f7b534e77f263d7be3acda6952ede3c2
ryosei15 <ryosei.315914.sari@gmail.com>
2024-04-27 17:53:15 +0900

    時間と曜日の変更の反映

---
+++ client/Hi/Hi/View/Account/Setting/TimeSettingModel.swift
@@ -0,0 +1,8 @@
+//
+//  TimeSettingModel.swift
+//  Hi
+//
+//  Created by ryosei on 2024/04/27.
+//
+
+import Foundation
---
+++ client/Hi/Hi/View/Account/Setting/TimeSettingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TimeSettingViewModel.swift
+//  Hi
+//
+//  Created by ryosei on 2024/04/27.
+//
+
+import Foundation
